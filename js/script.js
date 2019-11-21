b089a7bdb3b5b1253f82df23cc4a1894d2f61ad7
03DAICHI18 <dai.03.chi.18@gmail.com>
2019-11-21 20:39:26 +0900

    トップページアニメーション実装

---
+++ js/script.js
@@ -0,0 +1,13 @@
+// $(function(){
+//   $(window).scroll(function (){
+//     $('.effect-fade__text').each(function(){
+//         var elemPos = $(this).offset().top;
+//         var scroll = $(window).scrollTop();
+//         var windowHeight = $(window).height();
+//         if (scroll > elemPos - windowHeight + 200){
+//             $(this).addClass('effect-action');
+//         }
+//     });
+//   });
+// });
+
