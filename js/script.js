8a88a8c3a33bd5ad12631e0de32a16f04cc87f84
03DAICHI18 <dai.03.chi.18@gmail.com>
2019-11-21 21:50:56 +0900

    スライドショー設定

--- js/script.js
+++ js/script.js
@@ -1,13 +1,28 @@
-// $(function(){
-//   $(window).scroll(function (){
-//     $('.effect-fade__text').each(function(){
-//         var elemPos = $(this).offset().top;
-//         var scroll = $(window).scrollTop();
-//         var windowHeight = $(window).height();
-//         if (scroll > elemPos - windowHeight + 200){
-//             $(this).addClass('effect-action');
-//         }
-//     });
-//   });
-// });
+$(function(){
+ // メインビジュアルスライド
+var page=0;
+var lastPage =parseInt($("#slideshow img").length-1);
+$("#slideshow img").css("display","none");
+$("#slideshow img").eq(page).css("display","block");
+
+function changePage(){
+  $("#slideshow img").fadeOut(1000);
+  $("#slideshow img").eq(page).fadeIn(1000);
+};
+
+var Timer;
+function startTimer(){
+Timer =setInterval(function(){  
+  if(page === lastPage){
+    page = 0;
+    changePage();
+    } else {
+      page ++;
+      changePage();
+      };
+        },3000);
+}
+
+startTimer();
+});
 
