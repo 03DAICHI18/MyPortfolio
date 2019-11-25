7ae5b2dfe59c64d5d7edc8a0d7c2a874b3f507cd
03DAICHI18 <dai.03.chi.18@gmail.com>
2019-11-25 21:17:17 +0900

    パララックスの修正中

--- js/script.js
+++ js/script.js
@@ -1,28 +1,30 @@
 $(function(){
- // メインビジュアルスライド
-var page=0;
-var lastPage =parseInt($("#slideshow li").length-1);
-$("#slideshow li").css("display","none");
-$("#slideshow li").eq(page).css("display","block");
-
-function changePage(){
-  $("#slideshow li").fadeOut(2000);
-  $("#slideshow li").eq(page).fadeIn(2000);
-};
-
-var Timer;
-function startTimer(){
-Timer =setInterval(function(){  
-  if(page === lastPage){
-    page = 0;
-    changePage();
-    } else {
-      page ++;
-      changePage();
-      };
-        },4000);
-}
-startTimer();
+// // パララックススライド
+// // 初めのページと最後のページを定義。初めのページは表示させ、それ以外のページを非表示にさせる
+//   var page=0;
+//   $("#slideshow .parallax__area").eq(page).css("display","block");
+//   var lastPage =parseInt($("#slideshow .parallax__area").length-1);
+//   $("#slideshow .parallax__area").css("display","none");
+// // スライドの切り替わるペース（2秒でフェードアウト・イン）
+//   function changePage(){
+//     $("#slideshow .parallax__area").fadeOut(2000);
+//     $("#slideshow .parallax__area").eq(page).fadeIn(2000);
+//   };
+// // 最後のページが表示されてたら初めに戻る（ページ0へ）、それ以外が表示されてたら＋1カウントしてページチェンジ
+// // ※setInterval…一定時間ごとに特定の処理を繰り返す
+// var Timer;
+// function startTimer(){
+// Timer =setInterval(function(){  
+//   if(page === lastPage){
+//     page = 0;
+//     changePage();
+//     } else {
+//       page ++;
+//       changePage();
+//       };
+//         },4000);
+//       }
+//       startTimer();
 
 });
 
