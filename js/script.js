30d9a28a90542cb60a0670afdf94846913260c19
03DAICHI18 <dai.03.chi.18@gmail.com>
2019-11-28 03:14:31 +0900

    パララックス再修正（フェードは未実装）、ヘッダーにjQuery実装

--- js/script.js
+++ js/script.js
@@ -1,30 +1,19 @@
 $(function(){
-// // パララックススライド
-// // 初めのページと最後のページを定義。初めのページは表示させ、それ以外のページを非表示にさせる
-//   var page=0;
-//   $("#slideshow .parallax__area").eq(page).css("display","block");
-//   var lastPage =parseInt($("#slideshow .parallax__area").length-1);
-//   $("#slideshow .parallax__area").css("display","none");
-// // スライドの切り替わるペース（2秒でフェードアウト・イン）
-//   function changePage(){
-//     $("#slideshow .parallax__area").fadeOut(2000);
-//     $("#slideshow .parallax__area").eq(page).fadeIn(2000);
-//   };
-// // 最後のページが表示されてたら初めに戻る（ページ0へ）、それ以外が表示されてたら＋1カウントしてページチェンジ
-// // ※setInterval…一定時間ごとに特定の処理を繰り返す
-// var Timer;
-// function startTimer(){
-// Timer =setInterval(function(){  
-//   if(page === lastPage){
-//     page = 0;
-//     changePage();
-//     } else {
-//       page ++;
-//       changePage();
-//       };
-//         },4000);
-//       }
-//       startTimer();
+    // トップ位置でフィックスすると色を透過させるヘッダー
+
+    var Header = $('#Header');
+    //Headerの位置    
+    var HeaderTop = Header.offset().top;
+    //スクロールするたびに実行
+    $(window).scroll(function () {
+        var winTop = $(this).scrollTop();
+        //スクロール位置がHeaderの位置より下だったらクラスfixedを追加
+        if (winTop >= HeaderTop + 50) {
+            Header.css('opacity' , '.8');
+        } else if (winTop <= HeaderTop + 50) {
+            Header.css('opacity' , '1');
+        }
+    });
 
 });
 
