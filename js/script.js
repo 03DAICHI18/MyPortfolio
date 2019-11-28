ac4d89dc42495bbb2e370de4317650d0c0a1ee3c
03DAICHI18 <dai.03.chi.18@gmail.com>
2019-11-28 14:21:13 +0900

    ヘッダー透過位置をABOUTトップに変更

--- js/script.js
+++ js/script.js
@@ -1,16 +1,18 @@
 $(function(){
-    // トップ位置でフィックスすると色を透過させるヘッダー
-
+// ・・・トップ位置でフィックスすると色を透過させるヘッダー・・・
     var Header = $('#Header');
-    //Headerの位置    
-    var HeaderTop = Header.offset().top;
-    //スクロールするたびに実行
+    var About = $('#About');
+    //ウィンドウ枠からのAbout位置を取得
+    var AboutTop = About.offset().top;
+
     $(window).scroll(function () {
+    // ウィンドウ枠からのスクロール量を取得
         var winTop = $(this).scrollTop();
-        //スクロール位置がHeaderの位置より下だったらクラスfixedを追加
-        if (winTop >= HeaderTop + 30) {
+        //Aboutがウィンドウ枠より上にきたらopacityで透過
+        if (winTop >= AboutTop) {
             Header.css('opacity' , '.8');
-        } else if (winTop <= HeaderTop + 30) {
+        //Aboutがウィンドウ枠より下であれば透過させない
+        } else if (winTop <= AboutTop) {
             Header.css('opacity' , '1');
         }
     });
