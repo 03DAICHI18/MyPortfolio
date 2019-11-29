494ef0c8c42845f052cc7d6e846c53fad9fd5237
03DAICHI18 <dai.03.chi.18@gmail.com>
2019-11-30 01:45:27 +0900

    コンタクトブロック行きのhref追記。スクロールイベント実装

--- js/script.js
+++ js/script.js
@@ -4,7 +4,6 @@ $(function(){
     var About = $('#About');
     //ウィンドウ枠からのAbout位置を取得
     var AboutTop = About.offset().top;
-
     $(window).scroll(function () {
     // ウィンドウ枠からのスクロール量を取得
         var winTop = $(this).scrollTop();
@@ -26,5 +25,13 @@ $(function(){
         });
     });
 
+// ・・・ページ内スクロール・・・
+$('a[href^="#"]').click(function(event) {
+    // クリックした要素のhref属性の値を取得
+    var anchor = $(this).attr("href"); 
+    // 表示位置を取得
+    var position = $(anchor).offset().top -100; 
+    // アニメーションの効果の作成
+    $('body,html').animate({ scrollTop : position }, 500); 
 });
-
+});
