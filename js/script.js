69a7f0dd0ff670a6ed911d1e8c23d051e50bd185
03DAICHI18 <dai.03.chi.18@gmail.com>
2019-11-30 01:57:00 +0900

    ヘッダータグからSkill削除、SPも一列配置に。全項目ページ内遷移設定。

--- js/script.js
+++ js/script.js
@@ -30,8 +30,9 @@ $('a[href^="#"]').click(function(event) {
     // クリックした要素のhref属性の値を取得
     var anchor = $(this).attr("href"); 
     // 表示位置を取得
-    var position = $(anchor).offset().top -100; 
+    var position = $(anchor).offset().top -50; 
     // アニメーションの効果の作成
     $('body,html').animate({ scrollTop : position }, 500); 
+    return false;
 });
 });
