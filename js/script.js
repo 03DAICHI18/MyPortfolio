d1c5d8dbf7ae97b0cf1004b889f1ec4c68f8a4fb
03DAICHI18 <dai.03.chi.18@gmail.com>
2019-12-01 10:20:51 +0900

    Font Awesomeの読み込みをjsからcssに変更、リストの擬似要素にも表示させるように

--- js/script.js
+++ js/script.js
@@ -26,13 +26,13 @@ $(function(){
     });
 
 // ・・・ページ内スクロール・・・
-$('a[href^="#"]').click(function(event) {
-    // クリックした要素のhref属性の値を取得
-    var anchor = $(this).attr("href"); 
-    // 表示位置を取得
-    var position = $(anchor).offset().top -50; 
-    // アニメーションの効果の作成
-    $('body,html').animate({ scrollTop : position }, 500); 
-    return false;
-});
+    $('a[href^="#"]').click(function(event) {
+        // クリックした要素のhref属性の値を取得
+        var anchor = $(this).attr("href"); 
+        // 表示位置を取得
+        var position = $(anchor).offset().top -50; 
+        // アニメーションの効果の作成
+        $('body,html').animate({ scrollTop : position }, 500); 
+        return false;
+    });
 });
