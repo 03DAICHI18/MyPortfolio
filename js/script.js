4aced78c55aafc7cfcb01a0a7391b1d533f955f0
03DAICHI18 <dai.03.chi.18@gmail.com>
2019-12-12 17:03:43 +0900

    Workのモーダルウィンドウ位置調整

--- js/script.js
+++ js/script.js
@@ -15,6 +15,7 @@ $(function(){
             Header.css('opacity' , '1');
         }
 
+// ・・・ブロックフェードイン・・・
         $('.fadeIn').each(function(){
             var elemPos = $(this).offset().top;
             var scroll = $(window).scrollTop();
@@ -25,7 +26,8 @@ $(function(){
         });
     });
 
-// ・・・ページ内スクロール・・・
+
+// ・・・・・ページ内スクロール・・・・・
     $('a[href^="#"]').click(function(event) {
         // クリックした要素のhref属性の値を取得
         var anchor = $(this).attr("href"); 
@@ -36,20 +38,47 @@ $(function(){
         return false;
     });
 
-// ・・・キャプチャ画像の説明文・・・
+
+// ・・・・・キャプチャ画像の説明文・・・・・
     // li要素をマウスオーバー
     $(".work__capture-img").hover(function(){
     // キャプション部分の表示：フェードイン
-    $(this).children(".caption").stop().fadeIn(300);
+        $(this).children(".caption").stop().fadeIn(500);
     // キャプションのテキスト位置：top70% から 40% へ移動
-    $(this).children(".caption").children("p").stop().animate({"top" : "40%"}, 300);
+        $(this).children(".caption").children(".modal-link").stop().animate({"top" : "40%"}, 500);
     // ポインタを変更
-    $(this).css('cursor','pointer');
+        $(this).css('cursor','pointer');
     
     }, function(){
     // キャプション部分の非表示：フェードアウト
-    $(this).children(".caption").stop().fadeOut(300);
+        $(this).children(".caption").stop().fadeOut(500);
     // キャプションのテキスト位置：top50% から 70%へ移動
-    $(this).children(".caption").children("p").stop().animate({"top":"70%"}, 300);
+        $(this).children(".caption").children(".modal-link").stop().animate({"top":"70%"}, 500);
     });
+
+
+// ・・・・・モーダルウィンドウ・・・・・
+    // クリックで表示制御
+    $('.open').click(function(){
+        $('.overlay, .modal-window').fadeIn(1000);
+        return false;
+    });
+        
+    $('.close').click(function(){
+        $('.overlay, .modal-window').fadeOut(500);
+        return false;
+    });
+
+    // ウィンドウを常に中央に配置
+    locateCenter();
+    $(window).resize(locateCenter);
+
+    function locateCenter() {
+    var w = $(window).width();
+    var h = $(window).height();
+    var cw = $('.modal-window').outerWidth();
+    var ch = $('.modal-window').outerHeight();
+
+    $('.modal-window').css({'left': ((w - cw) / 2) ,'top': ((h - ch) / 2) });
+    }
 });
