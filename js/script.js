663450d802779f6c0e6cd345b42d7488908e71d8
03DAICHI18 <dai.03.chi.18@gmail.com>
2019-12-19 03:31:19 +0900

    画面遷移時間調整

--- js/script.js
+++ js/script.js
@@ -93,7 +93,21 @@ $(".work__capture-img").hover(function(){
 
         $('.overlay , .modal-window').stop().fadeOut(500);
         return false;
-
     });
 
+
+    $(function() {
+        // ハッシュリンク(#)と別ウィンドウでページを開く場合はスルー
+        $('#Work .link-button').on('click', function(e){
+          e.preventDefault(); // ナビゲートをキャンセル
+          url = $(this).attr('href'); // 遷移先のURLを取得
+            if (url !== '') {
+            $('body').addClass('fadeout');  // bodyに class="fadeout"を挿入
+            setTimeout(function(){
+              window.location = url;  // 0.8秒後に取得したURLに遷移
+            }, 800);
+            }
+            return false;
+        });
+        });
 });
